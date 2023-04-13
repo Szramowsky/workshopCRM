c3d41522f165d1c04169ea01b77be5146b43b38a
aszramowski <aszramowski@match-trade.com>
2023-04-13 15:52:15 +0200

    Db connection-final update

--- src/main/java/com/example/workshopCRM/controller/HomepageController.java
+++ src/main/java/com/example/workshopCRM/controller/HomepageController.java
@@ -5,16 +5,22 @@ import com.example.workshopCRM.repository.CarRepo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class HomepageController {
 
     private CarRepo carRepo;
 
     public HomepageController(CarRepo carRepo) {
         this.carRepo = carRepo;
+    }
+
+    @GetMapping("/a")
+    public String test() {
+        return "working";
     }
 
     @GetMapping("")
